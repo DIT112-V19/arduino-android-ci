310cc46c2770c7f9f2d1122b327864fef6682981
Dimitris Platis <platisd@users.noreply.github.com>
2019-05-11 13:55:11 +0200

    Fetch API key and Secret from BuildConfig

--- android/app/src/main/java/platis/solutions/ci/example/MainActivity.java
+++ android/app/src/main/java/platis/solutions/ci/example/MainActivity.java
@@ -2,12 +2,22 @@ package platis.solutions.ci.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mApiKeyField;
+    TextView mApiSecretField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApiKeyField = findViewById(R.id.apiKeyText);
+        mApiSecretField = findViewById(R.id.apiSecretText);
+
+        mApiKeyField.setText("API key: " + BuildConfig.apiKey);
+        mApiSecretField.setText("API secret: " + BuildConfig.apiSecret);
     }
 }
